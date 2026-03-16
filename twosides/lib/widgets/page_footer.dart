4e0c33afe52966a25ffae7ef52ac4eeeb47adb02
ROUSTAN Thomas <thomasroustan@outlook.com>
2026-03-16 12:08:14 +0100

    added mouse cursor to page footer

--- twosides/lib/widgets/page_footer.dart
+++ twosides/lib/widgets/page_footer.dart
@@ -35,15 +35,78 @@ class _PageFooterState extends State<PageFooter> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(
+                              context, '${RoutingPageNames.booking}');
+                        },
+                        child: const Text(
+                          'Booking',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'NeueMetanaNext',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            height: 0.9,
+                          ),
+                        )),
+                  ),
+                ),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    child: TextButton(
+                        onPressed: null,
+                        //() {
+                        //  //Navigator.pushNamed(context, '${RoutingPageNames.booking}');
+                        //},
+                        child: Text(
+                          'Prod. Event',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'NeueMetanaNext',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            height: 0.9,
+                          ),
+                        )),
+                  ),
+                ),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    child: TextButton(
+                        onPressed: null,
+                        //() {
+                        //  //Navigator.pushNamed(context, '${RoutingPageNames.booking}');
+                        //},
+                        child: Text(
+                          'Admin',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'NeueMetanaNext',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            height: 0.9,
+                          ),
+                        )),
+                  ),
+                ),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
                   child: TextButton(
                       onPressed: () {
                         Navigator.pushNamed(
-                            context, '${RoutingPageNames.booking}');
+                            context, '${RoutingPageNames.about}');
                       },
                       child: const Text(
-                        'Booking',
+                        'About',
                         style: const TextStyle(
                           color: Colors.white,
                           fontFamily: 'NeueMetanaNext',
@@ -53,71 +116,21 @@ class _PageFooterState extends State<PageFooter> {
                         ),
                       )),
                 ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  child: TextButton(
-                      onPressed: null,
-                      //() {
-                      //  //Navigator.pushNamed(context, '${RoutingPageNames.booking}');
-                      //},
-                      child: const Text(
-                        'Prod. Event',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'NeueMetanaNext',
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          height: 0.9,
-                        ),
-                      )),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  child: TextButton(
-                      onPressed: null,
-                      //() {
-                      //  //Navigator.pushNamed(context, '${RoutingPageNames.booking}');
-                      //},
-                      child: const Text(
-                        'Admin',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'NeueMetanaNext',
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          height: 0.9,
-                        ),
-                      )),
-                ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '${RoutingPageNames.about}');
-                    },
-                    child: const Text(
-                      'About',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'NeueMetanaNext',
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        height: 0.9,
-                      ),
-                    )),
               ]),
         ),
         Padding(
-          padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+          padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
           child: Container(
             width: MediaQuery.of(context).size.width * 0.3,
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Padding(
+                  const Padding(
                     padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                    child: const Text(
+                    child: Text(
                       "Suivez nos news, concerts, releases et rejoignez notre newsletter",
-                      style: const TextStyle(
+                      style: TextStyle(
                         color: Colors.white,
                         fontFamily: 'NeueMetanaNext',
                         fontSize: 12,
@@ -127,61 +140,67 @@ class _PageFooterState extends State<PageFooter> {
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
                     child: Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.17,
-                              child: TextFormField(
-                                controller: _emailController,
-                                decoration: const InputDecoration(
-                                    filled: true,
-                                    fillColor: Colors.white,
-                                    border: OutlineInputBorder()),
-                                style: TextStyle(
-                                  fontSize: 15,
+                          MouseRegion(
+                            cursor: SystemMouseCursors.text,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.17,
+                                child: TextFormField(
+                                  controller: _emailController,
+                                  decoration: const InputDecoration(
+                                      filled: true,
+                                      fillColor: Colors.white,
+                                      border: OutlineInputBorder()),
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                          ElevatedButton(
-                            onPressed: () async {},
-                            style: ElevatedButton.styleFrom(
-                                side: const BorderSide(
-                                  color: Colors.white,
-                                  width: 2,
-                                ),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(100),
-                                ),
-                                backgroundColor: TwoSidesColors.primaryColor,
-                                minimumSize: const Size(10, 60)),
-                            child: Text(
-                              "S'abonner",
-                              style: const TextStyle(
-                                  fontSize: 18,
-                                  fontFamily: 'signika',
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
+                          MouseRegion(
+                            cursor: SystemMouseCursors.click,
+                            child: ElevatedButton(
+                              onPressed: () async {},
+                              style: ElevatedButton.styleFrom(
+                                  side: const BorderSide(
+                                    color: Colors.white,
+                                    width: 2,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  backgroundColor: TwoSidesColors.primaryColor,
+                                  minimumSize: const Size(10, 60)),
+                              child: const Text(
+                                "S'abonner",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontFamily: 'signika',
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
                             ),
                           ),
                         ]),
                   ),
-                  Column(children: [
-                    const Text(
+                  const Column(children: [
+                    Text(
                       "ALL RIGHTS RESERVED",
-                      style: const TextStyle(
+                      style: TextStyle(
                           fontSize: 18,
                           fontFamily: 'signika',
                           color: Colors.white,
                           fontWeight: FontWeight.bold),
                     ),
-                    const Text(
+                    Text(
                       "2026 ©",
-                      style: const TextStyle(
+                      style: TextStyle(
                           fontSize: 18,
                           fontFamily: 'signika',
                           color: Colors.white,
