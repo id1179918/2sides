9cf2fe5c0b3c3d58e4d5794997cd62e0c1d68423
ROUSTAN Thomas <thomasroustan@outlook.com>
2026-03-16 11:27:39 +0100

    fixing dl filename

--- twosides/lib/http/download_file.dart
+++ twosides/lib/http/download_file.dart
@@ -3,6 +3,6 @@ import 'dart:html' as html;
 void downloadFile(int asset_id, String artist_name) {
   final anchor =
       html.AnchorElement(href: "https://2sides-agency.fr/api/asset/${asset_id}")
-        ..setAttribute('download', "presskit-${artist_name}")
+        ..setAttribute('download', "presskit-${artist_name}.pdf")
         ..click();
 }
